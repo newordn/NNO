aa1fd4fa8e4d4c0ac49157a73ddf76b8a0260b82
Neword <newordn@gmail.com>
2019-03-16 18:06:45 +0100

    Create removeDuplicates.java

---
+++ removeDuplicates.java
@@ -0,0 +1,18 @@
+public ArrayList<T> removeDuplicates(ArrayList<T> list) 
+    { 
+  
+        // Create a new ArrayList 
+        ArrayList<T> newList = new ArrayList<T>(); 
+        // Traverse through the first list 
+        for (T element : list) { 
+  
+            // If this element is not present in newList 
+            // then add it 
+            if (!newList.contains(element)) { 
+  
+                newList.add(element); 
+            } 
+        } 
+        // return the new list 
+        return newList; 
+    } 
